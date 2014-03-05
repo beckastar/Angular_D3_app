2f0d514ba2681f0a61830cfffc088a94f88e53fe
Becka <rebecca.robbins.et@gmail.com>
2014-03-05 09:30:27 -0800

    basic angular structure

---
+++ js/tv-premieres-app/controllers/mainController.js
@@ -0,0 +1,19 @@
+app.controller("mainController", function($scope, $http){
+
+    $scope.apiKey = "288fe216aa43d2c75448bdb3b1aaaa47";
+    $scope.results = []
+    $scope.init =function() {
+    	var today = new Date();
+    	var apiDate = today.getFullYear() + ('0' + (today.getMonth() + 1)).slice(-2)+ "" + ("0" + today.getDate()).slice(-2);
+    	$http.jsonp('http://api.trakt.tv/calendar/premieres.json/' + $scope.apiKey + '/' + apiDate + '/' + 30 + '/?callback=JSON_CALLBACK').success(function(data) {
+            angular.forEach(data, function(value, index)){
+            	var date = value.date;
+            	angular.forEach(value.episodes, function(tvshow, index){
+            		tvshow.date = date;
+            		$scope.results.push(tvshow);
+            	});
+            });
+          }).error(function(error) {
+             });
+    	};
+    });
